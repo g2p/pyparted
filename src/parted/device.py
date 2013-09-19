g2p/pyparted

commit 0474f3f567c00ad1cdad1eeac14362fe365f0885

Fix up pychecker errors reported by Jenkins.
This isn't really a full test yet.  Pychecker may be giving a lot of
false positives.  At least the things like:

No class attribute (_FileSystem__fileSystem) found

Are false positive, but I don't know how to make pychecker ignore them.
Iv'e cleaned up the other things it has found, but I still haven't
actually tested it in use.  This is why these changes are going on the
master branch for now.

src/parted/device.py
@@ -25,11 +25,10 @@
 import math
 import warnings
 
-import _ped
 import parted
-from .disk import diskType
+from disk import diskType
 
-from .decorators import localeC
+from decorators import localeC
 
 class Device(object):
     """Device()

src/parted/disk.py
@@ -25,8 +25,8 @@
 import _ped
 import parted
 
-from .cachedlist import CachedList
-from .decorators import localeC
+from cachedlist import CachedList
+from decorators import localeC
 
 class Disk(object):
     """Disk()

src/parted/filesystem.py
@@ -25,7 +25,7 @@
 import _ped
 import parted
 
-from .decorators import localeC
+from decorators import localeC
 
 # XXX: add docstrings!
 

src/parted/__init__.py
@@ -49,17 +49,17 @@ from _ped import TimerException
 from _ped import UnknownDeviceException
 from _ped import UnknownTypeException
 
-from .alignment import Alignment
-from .constraint import Constraint
-from .device import Device
-from .disk import Disk
-from .disk import diskType
-from .disk import diskFlag
-from .filesystem import FileSystem
-from .filesystem import fileSystemType
-from .geometry import Geometry
-from .partition import Partition
-from .partition import partitionFlag
+from parted.alignment import Alignment
+from parted.constraint import Constraint
+from parted.device import Device
+from parted.disk import Disk
+from parted.disk import diskType
+from parted.disk import diskFlag
+from parted.filesystem import FileSystem
+from parted.filesystem import fileSystemType
+from parted.geometry import Geometry
+from parted.partition import Partition
+from parted.partition import partitionFlag
 
 # the enumerated types in _ped need to be available from here too
 from _ped import UNIT_SECTOR
@@ -123,7 +123,7 @@ from _ped import DISK_GPT_PMBR_BOOT
 from _ped import DISK_TYPE_EXTENDED
 from _ped import DISK_TYPE_PARTITION_NAME
 
-from .decorators import localeC
+from decorators import localeC
 
 if sys.version_info >= (3,):
     string_types = str

src/parted/alignment.py
@@ -24,9 +24,8 @@
 #
 
 import parted
-import _ped
 
-from .decorators import localeC
+from decorators import localeC
 
 class Alignment(object):
     """Alignment()

src/parted/cachedlist.py
@@ -69,10 +69,6 @@ class CachedList(Sequence):
         self.__rebuildList()
         return len(self._lst)
 
-    def __reversed__(self):
-        self.__rebuildList()
-        return self._lst.__reversed__()
-
     def __repr__(self):
         self.__rebuildList()
         return repr(self._lst)

src/parted/constraint.py
@@ -24,9 +24,8 @@
 #
 
 import parted
-import _ped
 
-from .decorators import localeC
+from decorators import localeC
 
 class Constraint(object):
     """Constraint()

src/parted/decorators.py
@@ -34,7 +34,7 @@ def localeC(fn):
             pass
 
     @functools.wraps(fn)
-    def new(*args, **kwds):
+    def new(self, *args, **kwds):
         oldlocale = locale.getlocale(locale.LC_MESSAGES)
         _setlocale('C')
         try:

commit 0fa0952ccedcf2b67701666c0a3262d407cedf48

Make relative imports explicit
Reverts some Python 3 breakage from 0474f3f567c00ad1cdad1eeac14362fe365f0885

src/parted/device.py
@@ -28,7 +28,7 @@ import warnings
 import parted
 import _ped
 
-from decorators import localeC
+from .decorators import localeC
 
 class Device(object):
     """Device()

src/parted/disk.py
@@ -25,8 +25,8 @@
 import _ped
 import parted
 
-from cachedlist import CachedList
-from decorators import localeC
+from .cachedlist import CachedList
+from .decorators import localeC
 
 class Disk(object):
     """Disk()

src/parted/filesystem.py
@@ -25,7 +25,7 @@
 import _ped
 import parted
 
-from decorators import localeC
+from .decorators import localeC
 
 # XXX: add docstrings!
 